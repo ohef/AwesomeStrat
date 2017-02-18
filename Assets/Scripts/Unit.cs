4042ec10eaac69dda5c279d394d88da5b84b7881
Omar <Omar@DESKTOP-DP6I0FQ>
2017-02-18 11:03:34 -0800

    Unit now has maxHP and members protected by acessors

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -1,24 +1,72 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using Assets.General.DataStructures;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Assets.Map
 {
+    public class IntChangedEvent : UnityEvent<int> { }
+
     [System.Serializable]
     public class Unit : MonoBehaviour
     {
-        public int HP;
+        [SerializeField]
+        private int m_HP;
+        public int HP {
+            get { return m_HP; }
+            set
+            {
+                if ( m_HP != value )
+                {
+                    m_HP = value;
+                    HPChanged.Invoke( m_HP );
+                    UnitChanged.Invoke();
+                }
+            }
+        }
+
+        [SerializeField]
+        private int m_MaxHP;
+        public int MaxHP
+        {
+            get { return m_MaxHP; }
+            set
+            {
+                if ( m_MaxHP != value )
+                {
+                    m_MaxHP = value;
+                    MaxHPChanged.Invoke( m_MaxHP );
+                    UnitChanged.Invoke( );
+                }
+            }
+        }
+
         public int Defense;
         public int Attack;
         public int MovementRange;
         public int AttackRange;
 
+        public IntChangedEvent HPChanged;
+        public IntChangedEvent MaxHPChanged;
+        public UnityEvent UnitChanged;
+
         void OnDrawGizmos()
         {
             Gizmos.DrawCube( this.transform.position, Vector3.one * 0.5f );
+        }
+
+        void Awake()
+        {
+            HPChanged = new IntChangedEvent();
+            MaxHPChanged = new IntChangedEvent();
+
+            if ( UnitChanged == null )
+                UnitChanged = new UnityEvent();
+        }
+
+        void Start()
+        {
+            UnitChanged.Invoke();
         }
     }
 }
