b14d59ceb43e0768edc888dfb227af44d7170411
Omar <Omar@DESKTOP-DP6I0FQ>
2017-03-02 22:39:38 -0800

    added general coroutine helper class

---
+++ Assets/General/UnityExtensions/CoroutineHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.General.UnityExtensions
+{
+    public static class CoroutineHelper
+    {
+        public static IEnumerator AddBefore( IEnumerator enumerator, Action DoBefore )
+        {
+            DoBefore();
+            yield return enumerator;
+        }
+
+        public static IEnumerator AddActions( IEnumerator enumerator, Action DoBefore, Action DoAfter )
+        {
+            DoBefore();
+            yield return enumerator;
+            DoAfter();
+        }
+
+        public static IEnumerator AddAfter( IEnumerator enumerator, Action DoAfter )
+        {
+            yield return enumerator;
+            DoAfter();
+        }
+    }
+}
