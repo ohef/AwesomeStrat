e724f394d3c89ca55860b1e4ceef3f17e412922d
Omar <Omar@DESKTOP-DP6I0FQ>
2017-03-20 21:13:39 -0700

    Fixing push ability bugs, made GetTargetPredicate virtual

--- Assets/Scripts/Abilities/PushAbility.cs
+++ Assets/Scripts/Abilities/PushAbility.cs
@@ -1,8 +1,10 @@
 ﻿using Assets.General;
+using Assets.General.DataStructures;
 using Assets.General.UnityExtensions;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PushAbility : TargetAbility
 {
@@ -12,13 +14,36 @@ public class PushAbility : TargetAbility
         Range = 1; 
     }
 
+    public override Predicate<Unit> GetTargetPredicate( TurnState context )
+    {
+        return
+        delegate ( Unit target )
+        {
+            GameTile finalPoint = GetComputedPushTile( target );
+            return finalPoint == null ? false :
+            BattleSystem.Instance.Map.Occupied( finalPoint ) == false 
+            && base.GetTargetPredicate( context )( target );
+            //return BattleSystem.Instance.Map.Occupied( finalPoint ) == false && base.GetTargetPredicate( context )( target );
+        };
+    }
+
+    private GameTile GetComputedPushTile( Unit target )
+    {
+        BattleSystem sys = BattleSystem.Instance;
+        Vector2Int direction = sys.Map.UnitGametileMap[ target ].Position - sys.Map.UnitGametileMap[ Owner ].Position;
+        Vector2Int computedPoint = sys.Map.UnitGametileMap[ target ].Position + direction;
+
+        if ( sys.Map.IsOutOfBounds( computedPoint ) == false )
+            return sys.Map[ computedPoint ];
+        else
+            return null;
+    }
+
     public override void ExecuteOnTarget( Unit target )
     {
-        var sys = BattleSystem.Instance;
-        var direction = sys.Map.UnitGametileMap[ target ].Position - sys.Map.UnitGametileMap[ Owner ].Position;
-        var finalPoint = sys.Map[ sys.Map.UnitGametileMap[ target ].Position + direction ];
+        GameTile finalPoint = GetComputedPushTile( target );
 
-        sys.Map.PlaceUnit( target, finalPoint );
+        BattleSystem.Instance.Map.PlaceUnit( target, finalPoint );
         target.StartCoroutine( CustomAnimation.MotionTweenLinear( target.transform, finalPoint.Position.ToVector3(), 0.11f ) );
     }
 }
