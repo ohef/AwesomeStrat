6a04075342da287b58b27b3b1921adee4ec31ca3
Omar <Omar@DESKTOP-DP6I0FQ>
2017-03-30 15:38:58 -0700

    Undocommand action removed from battlesystem

---
+++ Assets/Scripts/UndoCommandAction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public struct UndoCommandAction : IUndoCommand
+{
+    private Action execute;
+    private Action undo;
+
+    public UndoCommandAction( Action execute, Action undo )
+    {
+        this.execute = execute;
+        this.undo = undo;
+    }
+
+    public void Execute()
+    {
+        execute();
+    }
+
+    public void Undo()
+    {
+        undo();
+    }
+}
