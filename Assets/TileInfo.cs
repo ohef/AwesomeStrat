584b401b1d1a5a28fecab8024aad2fa6b429793d
Omar <Omar@DESKTOP-DP6I0FQ>
2017-03-31 11:52:13 -0700

    Added a tileinfo panel

---
+++ Assets/TileInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileInfo : MonoBehaviour {
+    public ValueLabel TileCost;
+
+    public void UpdateInfo()
+    {
+        TileCost.UpdateValue( BattleSystem.Instance.Cursor.CurrentTile.CostOfTraversal );
+    }
+}
