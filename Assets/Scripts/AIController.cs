9ec14ade3ccadaf74355f5c48092a618e2ecf0a9
Omar <Omar@DESKTOP-DP6I0FQ>
2017-04-12 12:52:11 -0700

    AI code fixed some more, still monolithic and bad

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -28,15 +28,62 @@ internal class AIController : TurnController
         public int Visit( TargetAbility ability )
         {
             GameMap map = BattleSystem.Instance.Map;
+
             IEnumerable<Unit> interactableUnits =
-                ability.GetInteractableUnits(
-                    map.GetUnitsWithinRange( PositionForCheck, ability.Range ),
-                    ability.GetTargetPredicate( Context ) );
+                    map.GetUnitsWithinRange( PositionForCheck, ability.Range )
+                       .Where( ability.CanTargetFunction( Context ) );
+
             if ( interactableUnits.Count() == 0 )
                 return 0;
             else return interactableUnits.Count();
         }
     }
+
+    //public override void Enter( BattleSystem state )
+    //{
+    //    GameMap map = BattleSystem.Instance.Map;
+    //    //Get ordering for Units
+    //    foreach ( var myUnit in ControlledUnits )
+    //    {
+    //        IEnumerable<Vector2Int> unitMovement = map.GetValidMovementPositions( myUnit );
+    //        Unit bestTarget = GetBestTarget( GetEnemies() );
+    //        Vector2Int bestTargetPosition = map.UnitPos[ bestTarget ];
+    //        IEnumerable<Vector2Int> tilesAround = map.GetTilesWithinAbsoluteRange(
+    //            bestTargetPosition,
+    //            ( ( AttackAbility )myUnit.Abilities.First( ability => ability is AttackAbility ) ).Range );
+
+    //        IEnumerable<Vector2Int> pathToAttackPosition = null;
+    //        foreach ( var viableAttack in tilesAround )
+    //        {
+    //            pathToAttackPosition = MapSearcher.Search( map.UnitPos[ myUnit ], viableAttack, map );
+    //            if ( pathToAttackPosition != null )
+    //                break;
+    //        }
+
+    //        if ( pathToAttackPosition == null )
+    //            continue;
+
+    //        //Vector2Int attackPosition = tilesAround.First( pos => MapSearcher.Search( map.UnitPos[ myUnit ], pos, map ) != null );
+    //        //IEnumerable<Vector2Int> pathToAttackPosition = MapSearcher.Search( map.UnitPos[ myUnit ], attackPosition, map );
+    //        int pathLength = pathToAttackPosition.Count();
+    //        Debug.Log( pathToAttackPosition.Select( pos => pos.ToString() ).Aggregate( ( acc, val ) => string.Format( "{0},{1}", acc, val ) ) );
+
+    //        if ( pathLength > myUnit.MovementRange )
+    //            pathToAttackPosition = pathToAttackPosition.Take( myUnit.MovementRange );
+
+    //        //Vector2Int bestPosition = unitMovement.Select(
+    //        //    delegate ( Vector2Int pos )
+    //        //    {
+    //        //        AIAbilityScorer scorer = new AIAbilityScorer { PositionForCheck = pos, Context = this };
+    //        //        return new { Score = myUnit.Abilities.Select( ability => ability.Accept( scorer ) ).OrderByDescending( i => i ).First(), Position = pos };
+    //        //    } ).OrderBy( item => item.Score ).First().Position;
+
+    //        BattleSystem.Instance.CreateMoveCommand( pathToAttackPosition, myUnit ).Execute();
+    //        AttackAbility attackAbility = myUnit.Abilities.First( ability => ability is AttackAbility ) as AttackAbility;
+    //        attackAbility.ExecuteOnTarget( bestTarget );
+    //    }
+    //    BattleSystem.Instance.EndTurn();
+    //}
 
     public override void Enter( BattleSystem state )
     {
@@ -44,43 +91,49 @@ internal class AIController : TurnController
         //Get ordering for Units
         foreach ( var myUnit in ControlledUnits )
         {
-            IEnumerable<Vector2Int> unitMovement = map.GetValidMovementPositions( myUnit );
             Unit bestTarget = GetBestTarget( GetEnemies() );
-            Vector2Int bestTargetPosition = map.UnitPos[ bestTarget ];
-            IEnumerable<Vector2Int> tilesAround = map.GetTilesWithinAbsoluteRange(
-                bestTargetPosition,
-                ( ( AttackAbility )myUnit.Abilities.First( ability => ability is AttackAbility ) ).Range );
-
-            IEnumerable<Vector2Int> pathToAttackPosition = null;
-            foreach ( var pos in tilesAround )
-            {
-                pathToAttackPosition = MapSearcher.Search( map.UnitPos[ myUnit ], pos, map );
-                if ( pathToAttackPosition != null )
-                    break;
-            }
-
-            if ( pathToAttackPosition == null )
-                continue;
-
-            //Vector2Int attackPosition = tilesAround.First( pos => MapSearcher.Search( map.UnitPos[ myUnit ], pos, map ) != null );
-            //IEnumerable<Vector2Int> pathToAttackPosition = MapSearcher.Search( map.UnitPos[ myUnit ], attackPosition, map );
-            int pathLength = pathToAttackPosition.Count();
-
-            if ( pathLength > myUnit.MovementRange )
-                pathToAttackPosition = pathToAttackPosition.Take( myUnit.MovementRange );
-
-            //Vector2Int bestPosition = unitMovement.Select(
-            //    delegate ( Vector2Int pos )
-            //    {
-            //        AIAbilityScorer scorer = new AIAbilityScorer { PositionForCheck = pos, Context = this };
-            //        return new { Score = myUnit.Abilities.Select( ability => ability.Accept( scorer ) ).OrderByDescending( i => i ).First(), Position = pos };
-            //    } ).OrderBy( item => item.Score ).First().Position;
-
-            BattleSystem.Instance.CreateMoveCommand( pathToAttackPosition, myUnit ).Execute();
+            Vector2Int positionMoved = DoMove( myUnit, bestTarget );
             AttackAbility attackAbility = myUnit.Abilities.First( ability => ability is AttackAbility ) as AttackAbility;
-            attackAbility.ExecuteOnTarget( bestTarget );
+
+            Unit targetInSight = map.GetUnitsWithinRange( positionMoved, attackAbility.Range )
+                                    .Where( attackAbility.CanTargetFunction( this ) )
+                                    .FirstOrDefault( unit => unit == bestTarget );
+
+            if ( targetInSight != null )
+                attackAbility.ExecuteOnTarget( bestTarget );
         }
         BattleSystem.Instance.EndTurn();
+    }
+
+    private Vector2Int DoMove( Unit myUnit, Unit bestTarget )
+    {
+        GameMap                 map                = BattleSystem.Instance.Map;
+        Vector2Int              bestTargetPosition = map.UnitPos[ bestTarget ];
+        IEnumerable<Vector2Int> tilesAround        = 
+            map.GetTilesWithinAbsoluteRange( bestTargetPosition,
+            ( myUnit.Abilities.First( ability => ability is AttackAbility ) as AttackAbility ).Range )
+            .Where( pos => map.Occupied( pos ) == false && pos != bestTargetPosition );
+
+        IEnumerable<Vector2Int> pathToAttackPosition = null;
+
+        foreach ( var viableAttack in tilesAround )
+        {
+            pathToAttackPosition = MapSearcher.Search( map.UnitPos[ myUnit ], viableAttack, map );
+            if ( pathToAttackPosition != null )
+                break;
+        }
+
+        if ( pathToAttackPosition == null )
+            return map.UnitPos[ myUnit ];
+
+        if ( pathToAttackPosition.Count() > myUnit.MovementRange )
+        {
+            pathToAttackPosition = pathToAttackPosition.Take( myUnit.MovementRange ).ToList();
+            int pathCost = pathToAttackPosition.Select( pos => map.TilePos[ pos ] ).Sum( tile => tile.CostOfTraversal );
+        }
+
+        BattleSystem.Instance.CreateMoveCommand( pathToAttackPosition, myUnit ).Execute();
+        return pathToAttackPosition.Last();
     }
 
     private IEnumerable<Unit> GetEnemies()
@@ -104,50 +157,50 @@ internal class AIController : TurnController
     }
 }
 
-public interface IDecisionTreeNode
-{
-    IDecisionTreeNode MakeDecision();
-}
+//public interface IDecisionTreeNode
+//{
+//    IDecisionTreeNode MakeDecision();
+//}
 
-public abstract class Decision<T> : IDecisionTreeNode
-{
-    public IDecisionTreeNode TrueNode;
-    public IDecisionTreeNode FalseNode;
-    public T TestValue;
-    public abstract bool GetBranch();
-    public IDecisionTreeNode MakeDecision()
-    {
-        if ( GetBranch() )
-            return TrueNode == null ? null : TrueNode.MakeDecision();
-        else
-            return FalseNode == null ? null : FalseNode.MakeDecision();
-    }
-}
+//public abstract class Decision<T> : IDecisionTreeNode
+//{
+//    public IDecisionTreeNode TrueNode;
+//    public IDecisionTreeNode FalseNode;
+//    public T TestValue;
+//    public abstract bool GetBranch();
+//    public IDecisionTreeNode MakeDecision()
+//    {
+//        if ( GetBranch() )
+//            return TrueNode == null ? null : TrueNode.MakeDecision();
+//        else
+//            return FalseNode == null ? null : FalseNode.MakeDecision();
+//    }
+//}
 
-public class CanAttack : Decision<Unit>
-{
-    public override bool GetBranch()
-    {
-        //GameTile unitTile = BattleSystem.Instance.Map.UnitGametileMap[ TestValue ];
-        return true;
-    }
-}
+//public class CanAttack : Decision<Unit>
+//{
+//    public override bool GetBranch()
+//    {
+//        //GameTile unitTile = BattleSystem.Instance.Map.UnitGametileMap[ TestValue ];
+//        return true;
+//    }
+//}
 
-public abstract class ActionNode<T> : IDecisionTreeNode
-{
-    public T ActionValue; 
-    public IDecisionTreeNode MakeDecision()
-    {
-        return this;
-    }
+//public abstract class ActionNode<T> : IDecisionTreeNode
+//{
+//    public T ActionValue; 
+//    public IDecisionTreeNode MakeDecision()
+//    {
+//        return this;
+//    }
 
-    public abstract void Execute();
-}
+//    public abstract void Execute();
+//}
 
-public class DebugAction : ActionNode<object>
-{
-    public override void Execute()
-    {
-        Debug.Log( "Hey it's a debug action" );
-    }
-}
+//public class DebugAction : ActionNode<object>
+//{
+//    public override void Execute()
+//    {
+//        Debug.Log( "Hey it's a debug action" );
+//    }
+//}
