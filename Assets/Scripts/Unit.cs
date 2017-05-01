c15e3823af0bef50248b479bc478fa0ad6d630f3
Omar <Omar@DESKTOP-DP6I0FQ>
2017-04-30 19:09:41 -0700

    First addition of element stats

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -8,7 +8,8 @@ using UnityEngine.EventSystems;
 public class IntChangedEvent : UnityEvent<int> { }
 
 [RequireComponent( typeof( WaitAbility ) )]
-public class Unit : MonoBehaviour, IUnitEventsHandler
+public class Unit : MonoBehaviour, IUnitDamagedHandler
+//public class Unit : MonoBehaviour, IUnitDamagedHandler, IEffectProvider
 {
     [SerializeField]
     private int m_HP;
@@ -20,7 +21,7 @@ public class Unit : MonoBehaviour, IUnitEventsHandler
             if ( m_HP != value )
             {
                 m_HP = value;
-                UnitChanged.Invoke();
+                UnitChanged( this );
             }
         }
     }
@@ -35,38 +36,78 @@ public class Unit : MonoBehaviour, IUnitEventsHandler
             if ( m_MaxHP != value )
             {
                 m_MaxHP = value;
-                UnitChanged.Invoke();
+                UnitChanged( this );
             }
         }
     }
 
-    public int Defense;
+    public int Green
+    {
+        get { return green; }
+
+        set { green = value; }
+    }
+
+    public int Black
+    {
+        get { return black; }
+
+        set { black = value; }
+    }
+
+    public int Red
+    {
+        get { return red; }
+
+        set { red = value; }
+    }
+
+    public int Blue
+    {
+        get { return blue; }
+
+        set { blue = value; }
+    }
+
+    public int White
+    {
+        get { return white; }
+
+        set { white = value; }
+    }
+
+    [SerializeField]
+    private int green;
+    [SerializeField]
+    private int black;
+    [SerializeField]
+    private int red;
+    [SerializeField]
+    private int blue;
+    [SerializeField]
+    private int white;
+
+    public int AttackPower { get { return Black + Red + White + Blue + Green; } }
+
     public int MovementRange;
 
     [HideInInspector]
-    public List<Ability> Abilities = new List<Ability>(); 
+    public List<Ability> Abilities = new List<Ability>();
 
-    public UnityEvent UnitChanged;
-
-    public Animator Animator;
+    public event Action<Unit> UnitChanged;
 
     public void Awake()
     {
-        if ( UnitChanged == null )
-            UnitChanged = new UnityEvent();
-
         Abilities = GetComponents<Ability>().ToList();
-        Animator = this.GetComponentInChildren<Animator>();
     }
 
     public void Start()
     {
-        UnitChanged.Invoke();
+        UnitChanged( this );
     }
 
     public void UnitDamaged( int preDamage )
     {
-        //Animator.SetTrigger( "Damaged" );
         if ( HP <= 0 ) Die();
     }
 
@@ -81,3 +122,38 @@ public class Unit : MonoBehaviour, IUnitEventsHandler
         GetComponentInChildren<SpriteRenderer>().color = controller.PlayerColor;
     }
 }
+
+//public interface IEffectProvider
+//{
+//    int Green { get; set; }
+//    int Blue { get; set; }
+//    int Black { get; set; }
+//    int White { get; set; }
+//    int Red { get; set; }
+//}
+
+//public interface ICanApplyEffect
+//{
+//    void ApplyEffect( IEffectProvider effects );
+//}
+
+//public struct EffectBlock : IEffectProvider
+//{
+//    public int Green { get; set; }
+//    public int Blue  { get; set; }
+//    public int Black { get; set; }
+//    public int White { get; set; }
+//    public int Red   { get; set; }
+
+//    public static EffectBlock AddEffect( IEffectProvider b, IEffectProvider a )
+//    {
+//        return new EffectBlock
+//        {
+//            Green = a.Green + b.Green,
+//            Red   = a.Red   + b.Red,
+//            Blue  = a.Blue  + b.Blue,
+//            White = a.White + b.White,
+//            Black = a.Black + b.Black,
+//        };
+//    }
+//}
