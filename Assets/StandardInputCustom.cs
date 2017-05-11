f88b867b953c39933e880fba236a87cbb5124284
Omar <Omar@DESKTOP-DP6I0FQ>
2017-05-10 18:21:46 -0700

    Damage can now be displayed, input changed to allow no mouse

---
+++ Assets/StandardInputCustom.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class StandardInputCustom : StandaloneInputModule {
+    public override void Process()
+    {
+        bool flag = this.SendUpdateEventToSelectedObject();
+        if ( base.eventSystem.sendNavigationEvents )
+        {
+            if ( !flag )
+            {
+                flag |= this.SendMoveEventToSelectedObject();
+            }
+            if ( !flag )
+            {
+                this.SendSubmitEventToSelectedObject();
+            }
+        }
+        //if ( !this.ProcessTouchEvents() && base.input.mousePresent )
+        //{
+        //    this.ProcessMouseEvent();
+        //}
+    }
+}
