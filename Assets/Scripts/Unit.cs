a5e74a4fbb26ee5a43720a38e98fcb7d7f9b6460
Omar <Omar@DESKTOP-DP6I0FQ>
2017-05-13 15:10:58 -0700

    Making units simple now, idea for advance wars clone, maybe.

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
@@ -11,8 +12,9 @@ public interface IStartTurnHandler
 }
 
 [RequireComponent( typeof( WaitAbility ) )]
-public class Unit : MonoBehaviour, IUnitDamagedHandler, IStartTurnHandler
+public class Unit : MonoBehaviour, IUnitDamagedHandler
 {
+    [SerializeField]
     private int m_HP;
     public int HP
     {
@@ -22,11 +24,13 @@ public class Unit : MonoBehaviour, IUnitDamagedHandler, IStartTurnHandler
             if ( m_HP != value )
             {
                 m_HP = value;
-                UnitChanged( this );
+                if ( UnitChanged != null )
+                    UnitChanged( this );
             }
         }
     }
 
+    [SerializeField]
     private int m_MaxHP;
     public int MaxHP
     {
@@ -36,56 +40,11 @@ public class Unit : MonoBehaviour, IUnitDamagedHandler, IStartTurnHandler
             if ( m_MaxHP != value )
             {
                 m_MaxHP = value;
-                UnitChanged( this );
+                if ( UnitChanged != null )
+                    UnitChanged( this );
             }
         }
     }
-
-    public int Green
-    {
-        get { return green; }
-
-        set { green = value; }
-    }
-
-    public int Black
-    {
-        get { return black; }
-
-        set { black = value; }
-    }
-
-    public int Red
-    {
-        get { return red; }
-
-        set { red = value; }
-    }
-
-    public int Blue
-    {
-        get { return blue; }
-
-        set { blue = value; }
-    }
-
-    public int White
-    {
-        get { return white; }
-
-        set { white = value; }
-    }
-
-    [SerializeField]
-    private int green;
-    [SerializeField]
-    private int black;
-    [SerializeField]
-    private int red;
-    [SerializeField]
-    private int blue;
-    [SerializeField]
-    private int white;
 
     public int MovementRange;
 
@@ -94,12 +53,9 @@ public class Unit : MonoBehaviour, IUnitDamagedHandler, IStartTurnHandler
     [HideInInspector]
     public List<Ability> Abilities = new List<Ability>();
 
-    public int AttackPower { get { return Black / 2 + Red + White / 2 + Blue / 3 + Green; } }
-
     public void Awake()
     {
         Abilities = GetComponents<Ability>().ToList();
-        HP = MaxHP = Black * 2 + Red + White + Blue / 2 + Green * 3;
     }
 
     public void Start()
@@ -110,22 +66,6 @@ public class Unit : MonoBehaviour, IUnitDamagedHandler, IStartTurnHandler
     public void UnitDamaged( int preDamage )
     {
         if ( HP <= 0 ) Die();
-        else
-        {
-            PendingEffects.Add(
-                new EffectEntry
-                {
-                    CountDown = 2,
-                    OnFinish = () => HP = HP + preDamage >= MaxHP ? MaxHP : preDamage
-                } );
-        }
-    }
-
-    private List<EffectEntry> PendingEffects = new List<EffectEntry>();
-    class EffectEntry
-    {
-        public int CountDown;
-        public Action OnFinish;
     }
 
     public void Die()
@@ -137,16 +77,5 @@ public class Unit : MonoBehaviour, IUnitDamagedHandler, IStartTurnHandler
     public void RegisterTurnController( TurnController controller )
     {
         GetComponentInChildren<SpriteRenderer>().color = controller.PlayerColor;
-    }
-
-    public void OnTurnStart()
-    {
-        foreach ( var effect in PendingEffects )
-        {
-            effect.CountDown -= 1;
-            if ( effect.CountDown == 0 )
-                effect.OnFinish();
-        }
-        PendingEffects.RemoveAll( effect => effect.CountDown == 0 );
     }
 }
